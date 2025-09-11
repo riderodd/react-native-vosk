54c9882f223806519231fea884119c373a8e6309
riderodd <joris.gaudin@gmail.com>
2025-09-11 23:53:24 +0200

    feat: add expo support

---
+++ app.plugin.js
@@ -0,0 +1,3 @@
+// Root Expo config plugin entry so users can declare simply:
+// plugins: [ ["react-native-vosk", { models: [...], iOSMicrophonePermission: "..." }] ]
+module.exports = require('./plugin/withVosk.js');
